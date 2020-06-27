9151c19b99ad953d539e301c01359e2d843e424e
Damien <delmarle.damien@gmail.com>
2020-06-27 22:23:02 +0400

    decrease and increase hate in memory handler, list of friendly targets

--- station/Assets/RpgStation/Characters/Ai/CharacterMemoryHandler.cs
+++ station/Assets/RpgStation/Characters/Ai/CharacterMemoryHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Station
@@ -8,6 +9,7 @@ namespace Station
         #region FIELDS
         [SerializeField] private AreaTracker _interactionTracker = null;
         
+        private List<BaseCharacter> _friendlyTargets = new List<BaseCharacter>();
         private Dictionary<BaseCharacter, int> _hateMap = new Dictionary<BaseCharacter, int>();
         private BaseCharacter _owner;
         #endregion
@@ -33,20 +35,46 @@ namespace Station
         private bool OnDetect(Tracker tracker, Target target)
         {
             var characterComponent = target.Character;
-            if (characterComponent == null)
+            if (characterComponent == null || characterComponent == _owner)
             {
                 return false;
             }
 
-            var stance= _owner.ResolveStance(characterComponent);
+            var stance = _owner.ResolveStance(characterComponent);
+            switch (stance)
+            {
+                case Stance.Ally:
+                    if (_friendlyTargets.Contains(characterComponent) == false)
+                    {
+                        _friendlyTargets.Add(characterComponent);
+                    }
+                    break;
+                case Stance.Neutral:
+                    if (_hateMap.ContainsKey(characterComponent) == false)
+                    {
+                        _hateMap.Add(characterComponent, 0);
+                    }
+
+                    break;
+                case Stance.Enemy:
+                    if (_hateMap.ContainsKey(characterComponent) == false)
+                    {
+                        _hateMap.Add(characterComponent, 10);
+                    }
+                    break;
+            }
             Debug.Log($"found character: {characterComponent} stance: {stance.ToString()}");
-            //print("detect");
             return true;
         }
 
         private void OnUnDetect(Tracker tracker, Target target)
         {
-            //print("detect");
+            var characterComponent = target.Character;
+            if (characterComponent == null || characterComponent == _owner)
+            {
+                return;
+            }
+            
         }
         #endregion
 
@@ -55,18 +83,42 @@ namespace Station
 
         public void ReceiveHate(BaseCharacter source, int hateAmount)
         {
-            if (_hateMap.ContainsKey(source))
+            var stance = _owner.ResolveStance(source);
+            if (stance == Stance.Ally)
             {
-               // _hateMap[source]
+                Debug.LogError($"trying to add hate for a friendly player");
             }
             else
             {
-                _hateMap.Add(source, 1);
+                if (_hateMap.ContainsKey(source))
+                {
+                    _hateMap[source] += hateAmount;
+                }
+                else
+                {
+                    _hateMap.Add(source, hateAmount);
+                }
             }
         }
         
         public void DecreaseHate(BaseCharacter source, int hateAmount)
         {
+            var stance = _owner.ResolveStance(source);
+            if (stance == Stance.Ally)
+            {
+                Debug.LogError($"trying to remove hate for a friendly player");
+            }
+            else
+            {
+                if (_hateMap.ContainsKey(source))
+                {
+                    _hateMap[source] -= hateAmount;
+                }
+                else
+                {
+                    _hateMap.Add(source, 0);
+                }
+            }
         }
 
         #endregion
