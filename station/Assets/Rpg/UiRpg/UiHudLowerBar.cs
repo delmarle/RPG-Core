f49ebe89a6a2f36252f6b757708db20ccf8d5f12
Damien <delmarle.damien@gmail.com>
2023-03-24 00:48:51 +0100

    fixed currency ui

--- station/Assets/Rpg/UiRpg/UiHudLowerBar.cs
+++ station/Assets/Rpg/UiRpg/UiHudLowerBar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,41 @@ namespace Station
 {
     public class UiHudLowerBar : MonoBehaviour
     {
-    
+        [SerializeField] private UiCurrencyWidget _currencyWidget;
+        [SerializeField] private CurrencyModel _mainCurrency;
+        private PlayerInventorySystem _inventorySystem;
         
-        #region Buttons Calls
-        
+        #region Unity callbacks
+
+        private void Awake()
+        {
+            _inventorySystem = GameInstance.GetSystem<PlayerInventorySystem>();
+        }
+
+        private void OnEnable()
+        {
+            _inventorySystem.GetContainer().OnCurrencyChanged.AddListener(OnUpdateCurrency);
+            RefreshCurrencies();
+        }
+
+        private void OnDisable()
+        {
+            _inventorySystem.GetContainer().OnCurrencyChanged.RemoveListener(OnUpdateCurrency);
+        }
+
         #endregion
+        
+        private void OnUpdateCurrency(BaseItemContainer.CurrencyChange arg1, CurrencyModel arg2, long arg3, long arg4)
+        {
+            RefreshCurrencies();
+        }
+        
+        private void RefreshCurrencies()
+        {
+            var handler = _inventorySystem.GetContainer();
+            long amount = handler.GetCurrencyAmount(_mainCurrency);
+            _currencyWidget.DisplayAmount(_mainCurrency, amount, false);
+        }
     }
 }
 
